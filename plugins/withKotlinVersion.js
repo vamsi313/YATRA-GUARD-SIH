379fa32f68dd9edec52fde1f704c5f8a6dd70e55
Vamsi Krishna <vamsiklu367@gmail.com>
2026-08-28 15:29:11 +0530

    fix: remove enableBundleCompression for React Native 0.76 gradle compatibility

--- plugins/withKotlinVersion.js
+++ plugins/withKotlinVersion.js
@@ -1,27 +1,25 @@
-const { withProjectBuildGradle } = require('@expo/config-plugins');
+const { withProjectBuildGradle, withAppBuildGradle } = require('@expo/config-plugins');
 
 /**
- * Custom config plugin to force Kotlin 2.0.21 in android/build.gradle.
- * React Native 0.76 requires Kotlin 2.0.x (2.1+ breaks KotlinTopLevelExtension).
- * Kotlin 2.0.21 is fully supported by Expo 54 KSP matrix.
+ * Custom config plugin:
+ * 1. Forces Kotlin 2.0.21 in android/build.gradle.
+ * 2. Removes 'enableBundleCompression' from android/app/build.gradle for React Native 0.76 compatibility.
  */
-module.exports = function withKotlinVersion(config, { kotlinVersion = '2.0.21' } = {}) {
-  return withProjectBuildGradle(config, (config) => {
+module.exports = function withBuildFixes(config, { kotlinVersion = '2.0.21' } = {}) {
+  // 1. Patch root build.gradle
+  config = withProjectBuildGradle(config, (config) => {
     let contents = config.modResults.contents;
 
-    // Case 1: classpath with NO version (version catalog resolves it) - add explicit version
     contents = contents.replace(
       /classpath\(['"]org\.jetbrains\.kotlin:kotlin-gradle-plugin['"]\)/g,
       `classpath('org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}')`
     );
 
-    // Case 2: classpath WITH a version - replace the version
     contents = contents.replace(
       /classpath\(['"]org\.jetbrains\.kotlin:kotlin-gradle-plugin:[0-9.]+['"]\)/g,
       `classpath('org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}')`
     );
 
-    // Case 3: kotlinVersion in ext block
     contents = contents.replace(
       /kotlinVersion\s*=\s*["'][^"']+["']/g,
       `kotlinVersion = "${kotlinVersion}"`
@@ -30,4 +28,16 @@ module.exports = function withKotlinVersion(config, { kotlinVersion = '2.0.21' }
     config.modResults.contents = contents;
     return config;
   });
+
+  // 2. Patch app/build.gradle (Remove enableBundleCompression property unknown to RN 0.76)
+  config = withAppBuildGradle(config, (config) => {
+    let contents = config.modResults.contents;
+
+    contents = contents.replace(/.*enableBundleCompression.*/g, '');
+
+    config.modResults.contents = contents;
+    return config;
+  });
+
+  return config;
 };
